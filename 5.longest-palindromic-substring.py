f467fab5397eb60ea405d1a40d1204faeb115e3f
k-810 <k810marina@gmail.com>
2021-04-10 13:30:32 +0900

    5

---
+++ 5.longest-palindromic-substring.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        m = ''  # 回文
+        for i in range(len(s)):  # i = start
+            for j in range(len(s), i, -1):  # j = end
+                if len(m) >= j-i:  # To reduce time
+                    break
+                elif s[i:j] == s[i:j][::-1]:
+                    m = s[i:j]
+                    break
+        return m
+
+# @lc code=end
