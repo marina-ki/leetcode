2cf9fb0d2f431e3e38775b6081699c12068d7b40
k-810 <k810marina@gmail.com>
2021-04-10 12:57:56 +0900

    3

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=3 lang=python3
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = [0] * 128
+        left = right = 0
+        res = 0
+        while right < len(s):
+            r = s[right]
+            chars[ord(r)] += 1
+
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+
+            right += 1
+        return res
+
+        # @lc code=end
---
+++ memo.md
@@ -0,0 +1,11 @@
+## max, min
+
+min(a,b)
+max(a,b)
+
+## 文字を Unicode ポイントに変換
+
+print(ord('a')) //97
+print(ord('あ')) //12354
+
+アルファベット → 最大 128
